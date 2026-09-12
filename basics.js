3df1a8ba504e1ffe874ccf078c591372d8482470
coding-woffen <woffenjsx@proton.me>
2026-09-13 02:54:18 +0330

    Create basics.js

---
+++ basics.js
@@ -0,0 +1,29 @@
+// basics.js - Variables, types, and operators with console.log
+// Variables
+const name = 'Woffen';
+let age = 25;
+var greeting = 'Hello';
+// Data types
+const stringValue = 'Hello World';
+const numericValue = 42;
+const booleanValue = true;
+const nullValue = null;
+const undefinedValue = undefined;
+// Operators
+const sum = 10 + 5;
+const difference = 10 - 5;
+const product = 10 * 5;
+const division = 10 / 5;
+const remainder = 10 % 3;
+const power = 2 ** 10;
+console.log('Name:', name);
+console.log('Age:', age);
+console.log('Greeting:', greeting);
+console.log('String:', stringValue);
+console.log('Number:', numericValue);
+console.log('Boolean:', booleanValue);
+console.log('Sum:', sum);
+console.log('Product:', product);
+console.log('Division:', division);
+console.log('Remainder:', remainder);
+console.log('Power:', power);
